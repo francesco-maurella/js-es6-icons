e949453dfcc30b7ea9eca60b91f1369529e8af25
Francesco Maurella <francesco.maurella@live.it>
2021-01-21 17:55:50 +0100

    Inizio Milestone 2

--- js/script.js
+++ js/script.js
@@ -106,7 +106,7 @@ visualizzare in pagina tutte le icone di Icons con il proprio nome.
 icons.forEach((element, i) => {
   // creiamo elementi HTML
   $('#container').append('<div></div>'); // primo box
-  $('#container>div').eq(i).html('<i></i> <div></div>'); // interno box
+  $('#container>div').eq(i).html('<i></i> <div></div>').addClass('box');
   let thisIcon = $('#container>.box>i').eq(i); // recuperiamo icona attuale
   let thisTitle = $('#container>.box>div').eq(i);// recuperiamo titolo attuale
 
@@ -116,3 +116,26 @@ icons.forEach((element, i) => {
   thisIcon.addClass(`${family} ${prefix}${name}`); // agiamo sulla clase icona
   thisTitle.text(`${name}`); // agiamo sul titolo
 });
+
+/* MILESTONE 2
+Associare un diverso di colore ad ogni "tipo" di icona,
+attingendo da un array di colori.
+*/
+
+const colRed = '#F3280D';
+const colGreen = '#0DB70D';
+const colBlue = '#1642B0';
+
+const colors = [colRed, colGreen, colBlue];
+
+const iconsType = []; // dichiariamo array iconsType
+
+// copiamo le diverse proprietà "type" di icons in iconsType
+icons.forEach((element, i) => {
+  let {type} = element;
+  if (!iconsType.includes(type)) {
+    iconsType.push(type)
+  }
+});
+
+console.log(iconsType)

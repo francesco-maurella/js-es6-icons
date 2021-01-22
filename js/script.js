f3840127c61425125d7dfd65181d877d055baeb3
Francesco Maurella <francesco.maurella@live.it>
2021-01-22 03:01:56 +0100

    Miles 3 Completato

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
+// funzione inserisci elementi in HTML
+const innerElementIconHTML = ((item, index) => {
+  $('#icons-list').append('<div></div>'); // primo box
+  $('#icons-list>div').eq(index).html('<i></i> <div></div>').addClass('box');
+  let thisIcon = $('#icons-list>.box>i').eq(index); // recuperiamo icona attuale
+  let thisTitle = $('#icons-list>.box>div').eq(index);// recuperiamo titolo attuale
+
+  // destrutturiamo valori oggetto attuale
+  let {name, prefix, type, family} = item;
+
+  thisIcon.addClass(`${family} ${prefix}${name}`); // agiamo sulla clase icona
+  thisTitle.text(`${name}`); // agiamo sul titolo
+})
+
+
+
 // definizione array di oggetti "icone"
-const icons = [
+let icons = [
   {
     name : 'cat',
     prefix: 'fa-',
@@ -98,24 +114,16 @@ const icons = [
   },
 ];
 
+
 /* MILESTONE 1
 Utilizzando la funzione forEach e il template literal,
 visualizzare in pagina tutte le icone di Icons con il proprio nome.
 */
 
 icons.forEach((element, i) => {
-  // creiamo elementi HTML
-  $('#icons-list').append('<div></div>'); // primo box
-  $('#icons-list>div').eq(i).html('<i></i> <div></div>').addClass('box');
-  let thisIcon = $('#icons-list>.box>i').eq(i); // recuperiamo icona attuale
-  let thisTitle = $('#icons-list>.box>div').eq(i);// recuperiamo titolo attuale
-
-  // destrutturiamo valori oggetto attuale
-  let {name, prefix, type, family} = element;
-
-  thisIcon.addClass(`${family} ${prefix}${name}`); // agiamo sulla clase icona
-  thisTitle.text(`${name}`); // agiamo sul titolo
+  innerElementIconHTML(element, i)
 });
+
 
 /* MILESTONE 2
 Associare un diverso di colore ad ogni "tipo" di icona,
@@ -151,4 +159,48 @@ icons.forEach((element, i) => {
 icons.forEach((element, i) => {
   let {color} = element;
   $('.box').eq(i).css('color', `${color}`);
+});
+
+
+/* MILESTONE 3:
+Filtriamo le icone in base al tipo, tramite un Select
+popolato dinamicamente*/
+
+// recuperiamo elemento Select
+iconsTypes.forEach((element, i) => {
+  $('.type-select').append(`<option></option>`);
+  let thisOption = $('.type-select>option').eq(i+1);
+  thisOption.val(`${element}`).text(`${element}`);
+});
+
+// scriviamo nuova lista in base all'opzione scelta
+$('.type-select').change(function(){
+  let thisValue = $(this).val(); // opzione selezionata
+
+  iconsTypes.forEach((element, i) => {
+
+    if (thisValue === element) {
+      // filtriamo le icone in base al type
+      let selectedIcons = icons.filter((element) => {
+        let {type} = element
+        return type === thisValue;
+      });
+
+      // cancelliamo lista attuale
+      $('#icons-list').text('');
+
+      //inseriamo lista in HTML
+      selectedIcons.forEach((element, i) => {
+        innerElementIconHTML(element, i);
+        let {color} = element;
+        $('.box').eq(i).css('color', `${color}`);
+      });
+    } else if (!thisValue) {
+      icons.forEach((element, i) => {
+        innerElementIconHTML(element, i);
+        let {color} = element;
+        $('.box').eq(i).css('color', `${color}`);
+      });
+    }
+  });
 });
